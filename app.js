fb0ffcb712c1cf7377e20ef792f2db06e240307d
Federico <fedev93@gmail.com>
2019-03-24 19:36:03 +0100

    fourth commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -15,27 +14,92 @@ var app = express();
 
 myDB.connect((err)=>{
     if (err)
-        console.log(err);
+        throw err;
+    else
+        console.log("Database connesso!");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.post('/userExists', (req, res)=>{
+    myDB.query("SELECT * FROM Utente", (err, rows)=>{
+        if(err)
+            console.log(err);
+        else{
+            console.log("DB: ", rows, " length ", rows.length);
+            if(rows.length > 0){
+                res.send("200");
+            }
+            else{
+                res.send("404");
+            }
+        }
+    })
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/newUser', (req, res)=>{
+    var user = {
+        Nome: req.body.Nome,
+        Cognome: req.body.Cognome,
+        Email: req.body.Email
+    };
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    myDB.query("INSERT INTO Utente SET ?", user, (err, res )=>{
+       if (err)
+           console.log(err);
+       console.log(res);
+    });
+    res.end();
 });
 
-module.exports = app;
+app.post('/newIN', (req, res) =>{
+    myDB.query("SELECT DISTINCT idUtente FROM Utente", (err, rows)=>{
+       if(err)
+           throw err;
+       else{
+           var newEntrata = {
+               Utente: rows[0].idUtente,
+               Nome: req.body.Nome,
+               Data: req.body.Data,
+               Importo: req.body.Importo,
+               Categoria: req.body.Categoria
+           };
+           console.log(newEntrata);
+           myDB.query("INSERT INTO Entrata SET ?", newEntrata, (err, res )=>{
+               if (err)
+                   console.log(err);
+               console.log(res);
+           });
+       }
+    });
+    res.end();
+});
+
+app.post('/newOUT', (req, res) =>{
+    myDB.query("SELECT DISTINCT idUtente FROM Utente", (err, rows)=>{
+        if(err)
+            throw err;
+        else{
+            var newUscita = {
+                Utente: rows[0].idUtente,
+                Nome: req.body.Nome,
+                Data: req.body.Data,
+                Importo: req.body.Importo,
+                Categoria: req.body.Categoria
+            };
+            console.log(newUscita);
+            myDB.query("INSERT INTO Uscita SET ?", newUscita, (err, res )=>{
+                if (err)
+                    console.log(err);
+                console.log(res);
+            });
+        }
+    });
+    res.end();
+});
+
+app.listen(8080, () =>{
+    console.log("Server avviato sulla porta 8080!");
+});
--- public/JS/BM.js
+++ public/JS/BM.js
@@ -1,18 +1,51 @@
 
-
 $(document).ready(function() {
-    // if(!userExixts()){
-    //
-    // }
-    // else{
-    //
-    // }
-    $("#saveBtn").click(function () {
-        var   Nome= $("#inputNome").val().trim();
-        var    Cognome= $("#inputCognome").val().trim();
-        var   Email= $("#inputEmail").val().trim();
-        //send data to DB
-        $(location).attr("href", "../public/dashboard.html");
+    
+    fillGrid();
+    
+    $.post('/userExists', (res)=>{
+        console.log("res e'", res.valueOf());
+        if(res === "404" ){
+            alert("Devi prima inserire i dati dell'utente!");
+            $(location).attr("href", "welcome.html");
+        }
     });
 
+    $("#addIN").click(()=>{
+        var newEntrata = {
+            Nome: $("#nomeIn").val().trim(),
+            Data: $("#dataIn").val().trim(),
+            Importo: $("#importIn").val().trim(),
+            Categoria: $("#categoryIn").val().trim()
+        };
+
+        console.log("dati in input:  ", newEntrata);
+
+        $.post('/newIN', newEntrata, () =>{
+            alert("Entrata aggiunta con successo!");
+            location.reload();
+        });
+    });
+
+    $("#addOUT").click(()=>{
+        var newUscita = {
+            Nome: $("#nomeOut").val().trim(),
+            Data: $("#dateOut").val().trim(),
+            Importo: $("#importOut").val().trim(),
+            Categoria: $("#categoryOut").val().trim()
+        };
+
+        console.log("dati in input:  ", newUscita);
+
+        $.post('/newOUT', newUscita, () =>{
+            alert("Uscita aggiunta con successo!");
+            location.reload();
+        });
+    })
+
 });
+
+
+function fillGrid() {
+    
+}
